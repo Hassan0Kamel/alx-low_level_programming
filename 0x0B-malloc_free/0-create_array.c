64f478af56fb25ce71a261f8b4d31930295e7b18
Hassan0Kamel <Hassan.oka101@gmail.com>
2023-03-14 19:01:38 -0700

    automatically committed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 /**
- * * *create_array - creates an array of chars, 
- * and initializes it with a specific char.
+ * *create_array-creates an array of chars
+ *and initializes it with a specific char.
  *
  * *  @c: char to initialize
  *
