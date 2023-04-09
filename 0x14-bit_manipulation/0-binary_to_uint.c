d91b54f61db8422bd4c35bea56fe3a680d8a2ee4
Hassan0Kamel <Hassan.oka101@gmail.com>
2023-04-09 05:53:48 -0700

    nice Idea

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - convert a binary num to unsigned
+ * binary_to_uint - convert a binary num to unsigned
  * int
  * @b: string contains the binary num
  * Return: the converted num
