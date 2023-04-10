5ecd43e5f54f14e0a8568f454b0f7c3a4fc87698
Hassan0Kamel <Hassan.oka101@gmail.com>
2023-04-10 12:39:25 -0700

    noice

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -66,14 +66,14 @@ int main(int argc, char *argv[])
 	}
 
 	buffer = create_buffer(argv[2]);
-	from == open(argv[1], O_RDONLY);
+	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintfs(STDERR_FILENO,
+			dprintf(STDERR_FILENO,
 					"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintfs(STDERR_FILENO,
+			dprintf(STDERR_FILENO,
 					"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
