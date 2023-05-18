c04a73231d887e4056128b2fc66c933986d226fc
Hassan0Kamel <103977827+Hassan0Kamel@users.noreply.github.com>
2023-05-19 00:42:57 +0300

    Create 4-free_dlistintx.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistintx.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - Freeslistint_t list.
+ * @head: The head tint_t list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
