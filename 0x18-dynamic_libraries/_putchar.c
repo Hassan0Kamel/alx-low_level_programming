9865c3a93dc67eb9c6b1d4719c331d5a2f22f09a
Hassan0Kamel <hassan.oka101@gmail.com>
2023-05-22 12:06:05 -0700

    lmao

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: Taracter to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately
+ *_putchar - writes the character c to stdout
+ *@c: The character
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately
  */
 
 int _putchar(char c)
